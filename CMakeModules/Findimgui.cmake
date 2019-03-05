439e290aa8696981681c7da4d026bbd9b5d47628
will <shell_tdf@126.com>
2019-03-06 01:20:24 +0800

    fix bug

---
+++ CMakeModules/Findimgui.cmake
@@ -0,0 +1,17 @@
+
+FIND_PATH(imgui_INCLUDE_DIR imgui.h
+    PATHS
+    ${CMAKE_SOURCE_DIR}../install/include/
+    $ENV{PATH}
+    /usr/include/
+    /usr/local/include/
+)
+
+FIND_LIBRARY(imgui_LIBRARY 
+			NAMES imgui
+			PATHS ${imgui_INCLUDE_DIR}/../lib/)
+
+SET(imgui_FOUND "NO")
+IF(imgui_INCLUDE_DIR)
+    SET(imgui_FOUND "YES")
+ENDIF(imgui_INCLUDE_DIR)
